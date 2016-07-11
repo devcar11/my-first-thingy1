38ac8a8f6fe112891cec69819bbe45f7cc95c727
devcar11 <devcar03@gmail.com>
2016-07-11 18:54:23 -0400

    initial commit

---
+++ my first thingy/CommandVC.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Unturned.Player;
+using Rocket.Core.Logging;
+using Rocket.API;
+using Rocket.Unturned.Chat;
+
+namespace AdminTools
+{
+    class VC : IRocketCommand
+    {
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string> ();
+            }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get
+            {
+                return Rocket.API.AllowedCaller.Both;
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "Check if player is in vanish";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "vc";
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string> { "vc" };
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "<player>";
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length != 1)
+            {
+                UnturnedChat.Say(caller, "You must enter a player name!");
+                return;
+            }
+
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            if (player == null)
+            {
+                return;
+            }
+
+            UnturnedPlayer otherPlayer = UnturnedPlayer.FromName(command[0]);
+            if (otherPlayer == null)
+            {
+                UnturnedChat.Say(caller, "Could not find this player!");
+                return;
+            }
+
+            if (otherPlayer.Features.VanishMode)
+            {
+                UnturnedChat.Say(caller, "The player " + otherPlayer.DisplayName + " is in vanish");
+            }
+            else
+            {
+                UnturnedChat.Say(caller, "The player " + otherPlayer.DisplayName + " is not vanish");
+            }
+        }
+    }
+}
---
+++ my first thingy/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Core.Plugins;
+using Rocket.Core.Logging;
+
+namespace AdminTools
+{
+    class Plugin : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            Logger.Log("Plugin has loaded!");
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("Plugin has unloaded!");
+        }
+    }
+}
